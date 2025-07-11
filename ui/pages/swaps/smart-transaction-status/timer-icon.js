4a54ae5db414193bc957b045a380539cf340ed49
serbanloteanu5 <146815475+serbanloteanu5@users.noreply.github.com>
2025-07-11 06:59:18 +0200

    Create timer-icon.js

---
+++ ui/pages/swaps/smart-transaction-status/timer-icon.js
@@ -0,0 +1,20 @@
+export default function TimerIcon() {
+  return (
+    <svg
+      width={15}
+      height={14}
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.36621 0.081Q3.62 .08,0.58496,3.1T-.42,.8t-7,7l7-7v-5h-.9v5c-.2,-.3-.9,-.3-.9,.2l-4,4q.1,.2,.8,.2t.
+         8-,zZm-.,7Q.,a,Q,,ZM,
+          Q.,.,
+          t,q,t,
+           q,
+            ,,, zZm-q,zQ-t,zM-z,"
+        fill="--color-primary-default" />
+    </svg>
+);
+}
